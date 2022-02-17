8ea6c0007eecd771188cbf1b333a96a1023a816e
sanjibpaul59 <sanjibpaul171@gmail.com>
2022-02-17 17:59:03 +0600

    new pages created

---
+++ client/pages/auth/login-page.vue
@@ -0,0 +1,16 @@
+<template>
+  <Form :btnTask="btnTask" @LoginUser="userLogin" />
+</template>
+
+<script>
+export default {
+  data: () => ({
+    btnTask: "Login",
+  }),
+  methods: {
+    userLogin(value) {
+      console.log(value);
+    },
+  },
+};
+</script>
---
+++ client/pages/auth/register-page.vue
@@ -0,0 +1,16 @@
+<template>
+  <Form :btnTask="btnTask" @RegisterUser="userRegister" />
+</template>
+
+<script>
+export default {
+  data: () => ({
+    btnTask: "Register",
+  }),
+  methods: {
+    userRegister(value) {
+      console.log(value);
+    },
+  },
+};
+</script>
