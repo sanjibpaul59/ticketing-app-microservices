441d4cf6bfbf6d15ae27e976d818810b934aa7ef
sanjibpaul59 <sanjibpaul171@gmail.com>
2022-02-17 17:59:33 +0600

    default layout changed

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
   <v-app dark>
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title>
+        <nuxt-link class="display-1" to="/">{{ title }}</nuxt-link>
+      </v-toolbar-title>
       <v-spacer />
+      <nuxt-link class="mr-3" to="/auth/login-page">Login</nuxt-link>
+      <nuxt-link to="/auth/register-page">Register</nuxt-link>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -26,7 +30,64 @@ export default {
       fixed: false,
 
       title: "Ticketing App",
+      decoration: "none",
     };
   },
 };
 </script>
+
+<style>
+/* home route and active route will show in bold as it matches / and /about */
+a.nuxt-link-active {
+  font-weight: bold;
+}
+/* exact link will show the primary color for only the exact matching link */
+a.nuxt-link-exact-active {
+  color: #00c58e;
+}
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  margin: 0;
+}
+
+main {
+  margin: 0 auto;
+  padding: 0 1rem;
+  margin-top: 100px;
+  max-width: 1280px;
+  text-align: center;
+}
+img {
+  margin-bottom: 1rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+li {
+  margin: 0 0.5rem;
+  padding: 0.25rem;
+  font-size: 1.2rem;
+}
+
+nav {
+  padding: 0 1rem;
+}
+
+a,
+a:visited {
+  text-decoration: none;
+  color: #006064;
+}
+
+a:hover {
+  color: #00c58e;
+}
+</style>
