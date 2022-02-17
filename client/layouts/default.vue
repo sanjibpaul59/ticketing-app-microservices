2820bf1c8e19b4a29bf0eee7290348da6b99d461
sanjibpaul59 <sanjibpaul171@gmail.com>
2022-02-17 13:51:18 +0600

    nuxt ap setup

---
+++ client/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      clipped: false,
+
+      fixed: false,
+
+      title: "Ticketing App",
+    };
+  },
+};
+</script>
