ba16361218d6f77b85c69c39c6b3918e5ed2b728
sanjibpaul59 <sanjibpaul171@gmail.com>
2022-02-17 14:59:21 +0600

    removing serving errors

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -3,12 +3,16 @@ import colors from "vuetify/es5/util/colors";
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        titleTemplate: "%s - client",
-        title: "client",
+        titleTemplate: "%s - BUY/SELL Tickets Online",
+        title: "Ticket Share",
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: "" },
+            {
+                hid: "description",
+                name: "description",
+                content: "Online marketplace to BUY/SELL tickets of concerts, matchday etc.",
+            },
             { name: "format-detection", content: "telephone=no" },
         ],
         link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
